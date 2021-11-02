37567d204f04ce87777fabc902c59709c1ae1bf2
suhrobsaidov <suhrob.aas@gmail.com>
2021-11-02 11:22:26 +0500

    configuring db credentials

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,7 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use JWTAuth;
+use Tymon\JWTAuth\JWTAuth;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Tymon\JWTAuth\Exceptions\JWTException;
@@ -13,27 +14,19 @@ class ApiController extends Controller
 {
     public function register(Request $request)
     {
-        //Validate data
-        $data = $request->only('name', 'email', 'password');
+        $data = $request->only('login','password');
         $validator = Validator::make($data, [
-            'name' => 'required|string',
-            'email' => 'required|email|unique:users',
+            'login' => 'required|string',
             'password' => 'required|string|min:6|max:50'
         ]);
 
-        //Send failed response if request is not valid
         if ($validator->fails()) {
             return response()->json(['error' => $validator->messages()], 200);
         }
-
-        //Request is valid, create new user
         $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
+            'login' => $request->login,
             'password' => bcrypt($request->password)
         ]);
-
-        //User created, return success response
         return response()->json([
             'success' => true,
             'message' => 'User created successfully',
@@ -43,21 +36,16 @@ class ApiController extends Controller
 
     public function authenticate(Request $request)
     {
-        $credentials = $request->only('email', 'password');
-
-        //valid credential
+        $credentials = $request->only('login', 'password');
         $validator = Validator::make($credentials, [
-            'email' => 'required|email',
+            'login' => 'required|string',
             'password' => 'required|string|min:6|max:50'
         ]);
 
-        //Send failed response if request is not valid
         if ($validator->fails()) {
             return response()->json(['error' => $validator->messages()], 200);
         }
 
-        //Request is validated
-        //Crean token
         try {
             if (! $token = JWTAuth::attempt($credentials)) {
                 return response()->json([
@@ -72,8 +60,6 @@ class ApiController extends Controller
                 'message' => 'Could not create token.',
             ], 500);
         }
-
-        //Token created, return with success response and jwt token
         return response()->json([
             'success' => true,
             'token' => $token,
@@ -82,12 +68,9 @@ class ApiController extends Controller
 
     public function logout(Request $request)
     {
-        //valid credential
         $validator = Validator::make($request->only('token'), [
             'token' => 'required'
         ]);
-
-        //Send failed response if request is not valid
         if ($validator->fails()) {
             return response()->json(['error' => $validator->messages()], 200);
         }
--- app/Models/User.php
+++ app/Models/User.php
@@ -18,8 +18,7 @@ class User extends Authenticatable
      * @var string[]
      */
     protected $fillable = [
-        'name',
-        'email',
+        'login',
         'password',
     ];
 
@@ -38,7 +37,12 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
